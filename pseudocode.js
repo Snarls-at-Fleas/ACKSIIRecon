91261d7ed3b37ec472d648493fd4b74821f4136f
Pavel Berlin <berlin.pavel@gmail.com>
2024-06-23 20:42:42 +0300

    Add simple explanation of the app to pseudocode file

--- pseudocode.js
+++ pseudocode.js
@@ -1,5 +1,8 @@
 // This is pseudocode for the app
-// 
+// The app reads armies data from .json and checks their parameters against each other to find relevant modifier of the random roll.
+// Then it makes the roll and finds what data about the army it should give to commanders of other armies.
+// After that it outputs that data in clean format that can be copied and pasted to another app, messenger or text editor.
+//
 //
 //
 // open data.json for reading
